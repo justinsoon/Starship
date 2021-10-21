ec936d57c7b86c5123aa8fbd17c431c3a6c60192
Justin <justin.soon@verizon.net>
2021-10-21 19:12:11 -0400

    Modifier add / cleanup
    Bug with replace for type of modifier in overall program, but not when singled out. 
    Improved runtime.

--- SBUX Adder/SBUXAdd.py
+++ SBUX Adder/SBUXAdd.py
@@ -6,67 +6,74 @@ start_time = time.time()
 ################ Class Objects
 class GlobalProduct:
     itemName = ''
-    globalID = ''
+    info = ''
     def __init__(self, itemName, globalID):
         self.itemName = itemName
-        self.globalID =  globalID
-class BevProduct:
+        self.info = globalID
+class BarcodeProduct:
     itemName = ''
-    info = (0)
-    def __init__(self, itemName, Barcode):
+    info = ''
+    def __init__(self, itemName, barCode):
         self.itemName = itemName
-        self.info =  (Barcode)
-class FoodProduct:
-    itemName = ''
-    info = (0)
-    def __init__(self, itemName, Barcode):
-        self.itemName = itemName
-        self.info =  (Barcode)
+        self.info =  (barCode)
 class CalProduct:
-    productName = ''
+    itemName = ''
     info = ('', 0)
-    def __init__(self, productName, size, calories):
-        self.productName = productName
+    def __init__(self, itemName, size, calories):
+        self.itemName = itemName
         self.info =  (size, calories)
-
+        
+################ Functions
+######## Basic Reader
+def readCSV(fileName, rowSkip):
+    fileReader = open(fileName, "r", encoding='UTF-8')
+    for i in range(rowSkip):
+        fileReader.readline()
+    fileContent = fileReader.read()
+    return fileContent
+######## Store into its respective dictionary
+def dictStore(productArray, productDict):
+    for product in productArray:
+        if product.itemName in (productDict):
+            productDict[product.itemName].append(product.info)
+        else:
+            productDict[product.itemName] = [product.info]
+######## Merge two DFs
+def normMerge(firstDF, lastDF):
+    merge = firstDF.merge(lastDF, on='Name', how='right', suffixes=('', '_y'))
+    merge.drop(merge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
+    return merge
+######## Merge replaces old data so, this combines
+def barcodeDupeMerge(firstDF, lastDF):
+    merge = firstDF.merge(lastDF, on='Name', how='right', suffixes=('', '_y'))
+    merge = merge.replace(np.nan, '')
+    merge["Barcode"] = [''.join(i) for i in zip(merge["Barcode"].map(str),merge["Barcode_y"].map(str))]
+    merge.drop(merge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
+    return merge
 
 ################ Variables
-bevProducts = [] 
-bevProdDict = {} 
+bevProducts = []
+modProducts = []
 foodProducts = []
-foodProdDict = {}  
+barcodeProdDict = {}
 globalProducts = []
-globalProdDict = {}  
-caloriesProducts = [] 
-calProdDict = {} 
+globalProdDict = {}
+caloriesProducts = []
+calProdDict = {}
+removeType = ['Add', 'Extra', 'Light', 'Substitute','Sub', 'No ', ' add', ' extra', ' light', ' sub', ' regular', ' no', 
+    ' Modifier', ' Single', ' Double', ' Triple', 'Quad', 'Solo ', 'Doppio ', 'Triple ', ' modifier', ' modifier extra',
+    ' modifier light', ' modifier no', ' modifier regular', ' modifier sub', 'Extra ', 'Light ', 'Add ']
 removeSizes = ["Kids", "Short", "Tall", "Grande", "Venti", "Trenta"]
 
-################ Reading CSVs
-######## Reads Global ID 
-globalReader = open("Master.csv", "r", encoding='UTF-8')
-# skip first row
-globalReader.readline()    
-globalContent = globalReader.read()
-######## Beverages SKU
-bevReader = open("BevSKU.csv", "r", encoding='UTF-8')
-# skip first six rows
-for i in range(6):
-    bevReader.readline()
-bevContent = bevReader.read()
-######## Food SKU
-foodReader = open("FoodSKU.csv", "r", encoding='UTF-8')
-# skip first two rows
-for i in range(2):
-    foodReader.readline()    
-foodContent = foodReader.read()
-######## Calories Info
-calReader = open("Calories.csv", "r", encoding='UTF-8')
-# skip first row
-calReader.readline()
-calContent = calReader.read()
+#################################### Reading CSVs ####################################
+globalContent = readCSV('Master.CSV', 1)    # Global ID - skip first row
+bevContent = readCSV('BevSKU.CSV', 6)       # Beverages SKU - skip first six row
+modContent = readCSV('ModSKU.CSV', 6)       # Modifier SKU - skip first six row
+foodContent = readCSV('FoodSKU.CSV', 2)     # Food SKU- skip first two row
+calContent = readCSV('Calories.CSV', 1)     # Calories Info - skip first row
 
 ################ Storing Data
-######## Global        
+######## Global IDs       
 for globalLine in globalContent.split('\"\n\"'):
     globalColumns = globalLine.split(',')
     if len(globalColumns) > 4:
@@ -74,12 +81,8 @@ for globalLine in globalContent.split('\"\n\"'):
         globalID = globalColumns[0]
         globalID = globalID.replace('\"', '')
         globalProducts.append(GlobalProduct(globalName, globalID))
-for product in globalProducts:
-    if product.itemName in (globalProdDict):
-        globalProdDict[product.itemName].append(product.globalID)
-    else:
-        globalProdDict[product.itemName] = [product.globalID]
-######## Beverages
+dictStore(globalProducts, globalProdDict)
+######## Beverages 
 for bevLine in bevContent.split("\n"):
     bevColumns = bevLine.split(",")
     if len(bevColumns) > 2:
@@ -87,24 +90,28 @@ for bevLine in bevContent.split("\n"):
         for size in removeSizes:
             bevName = bevName.replace(size, '').strip()
         bevBarcode = bevColumns[0]
-        bevProducts.append(BevProduct(bevName, bevBarcode))
-for product in bevProducts:
-    if product.itemName in (bevProdDict):
-        bevProdDict[product.itemName].append(product.info)
-    else:
-        bevProdDict[product.itemName] = [product.info]
+        bevProducts.append(BarcodeProduct(bevName, bevBarcode))
+dictStore(bevProducts, barcodeProdDict)
+######## Modifiers
+for modLine in modContent.split("\n"):
+    modColumns = modLine.split(",")
+    if len(modColumns) > 2:
+        modName = modColumns[1]
+        for size in removeSizes:
+            modName = modName.replace(size, '').strip()
+        for type in removeType:
+            modName = modName.replace(type, '').strip()
+        modBarcode = modColumns[0]
+        modProducts.append(BarcodeProduct(modName, modBarcode))
+dictStore(modProducts, barcodeProdDict)
 ######## Food
 for foodLine in foodContent.split("\n"):
     foodColumns = foodLine.split(",")
     if len(foodColumns) > 2:
         foodName = foodColumns[0]
         foodBarcode = foodColumns[1]
-        foodProducts.append(FoodProduct(foodName, foodBarcode))
-for product in foodProducts:
-    if product.itemName in (foodProdDict):
-        foodProdDict[product.itemName].append(product.info)
-    else:
-        foodProdDict[product.itemName] = [product.info]
+        foodProducts.append(BarcodeProduct(foodName, foodBarcode))
+dictStore(foodProducts, barcodeProdDict)
 ######## Calories
 for calLine in calContent.split("\n"):
     calColumns = calLine.split(",")
@@ -113,11 +120,7 @@ for calLine in calContent.split("\n"):
         calSize = calColumns[3]
         calories = calColumns[4]
         caloriesProducts.append(CalProduct(calitemName, calSize, calories))
-for product in caloriesProducts:
-    if product.productName in (calProdDict):
-        calProdDict[product.productName].append(product.info)
-    else:
-        calProdDict[product.productName] = [product.info]
+dictStore(caloriesProducts, calProdDict)
 
 ################ Data Sort Formatting
 ######## Global 
@@ -127,64 +130,40 @@ for k,v in globalProdDict.items():
     for sv in v:    
         globalOutputString += sv
     globalOutputString += "\n"
-globalFormatted = io.StringIO(globalOutputString)  
-######## Beverages
-bevOutputString = "Name,Barcode\n"
-for k,v in bevProdDict.items():
-    bevOutputString += k + ","
+globalFormatted = io.StringIO(globalOutputString)
+######## Barcodes
+BarcodeOutputString = "Name,Barcode\n"
+for k,v in barcodeProdDict.items():
+    BarcodeOutputString += k + ","
     for sv in v:    
-        bevOutputString +=  sv + "&"  
-    bevOutputString = bevOutputString[:-1]  
-    bevOutputString += "\n"
-bevFormatted = io.StringIO(bevOutputString)  
-######## Food 
-foodOutputString = "Name,Barcode\n"
-for k,v in foodProdDict.items():
-    foodOutputString += k + ","
-    for sv in v:    
-        foodOutputString +=  sv + "&"  
-    foodOutputString = foodOutputString[:-1]  
-    foodOutputString += "\n"
-foodFormatted = io.StringIO(foodOutputString)      
+        BarcodeOutputString += sv + "&"  
+    BarcodeOutputString = BarcodeOutputString[:-1]  
+    BarcodeOutputString += "\n"
+barcodeFormatted = io.StringIO(BarcodeOutputString)
 ######## Calories
 calOutputString = "Name,Nutritional Data\n"
 calOutputString += ("\n".join("{},{}".format(k, v) for k, v in calProdDict.items()))
-calOutputString = calOutputString.replace(', ',' ').replace("'","").replace("'","").replace("[","").replace("]","").replace("(","").replace(")","")
+calOutputString = calOutputString.replace(', ',' ').replace('\') (\'',' Calories - ').replace("\' \'"," ").replace('[(\'','').replace("')]"," Calories")
 calFormatted = io.StringIO(calOutputString)
 
-################ Data into DataFrame
-######## Beverages
-bevDF = pd.read_csv(bevFormatted, sep=',')
-######## Food
-foodDF = pd.read_csv(foodFormatted, sep=',')
-######## Global
-globalDF = pd.read_csv(globalFormatted, sep=',')
-######## Calories
-calDF = pd.read_csv(calFormatted, sep=',')
-######## Final Spreadsheet
-finalDF = pd.read_csv('final.csv', encoding='UTF-8')
+############################ Data into DataFrame ############################
+globalDF = pd.read_csv(globalFormatted, sep=',')        # Global
+barcodeDF = pd.read_csv(barcodeFormatted, sep=',')      # Barcodes
+calDF = pd.read_csv(calFormatted, sep=',')              # Calories
+finalDF = pd.read_csv('final.csv', encoding='UTF-8')    # Final Spreadsheet
 
-################ Combining Data Into A Single Sheet
-######## Global ID
-globalMerge = globalDF.merge(finalDF, on='Name', how='right', suffixes=('', '_y'))
-globalMerge.drop(globalMerge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
-######## Beverages
-bevMerge = bevDF.merge(globalMerge, on='Name', how='right', suffixes=('', '_y'))
-bevMerge.drop(bevMerge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
-######## Food
-foodMerge = foodDF.merge(bevMerge, on='Name', how='right', suffixes=('', '_y'))
-foodMerge = foodMerge.replace(np.nan, '')
-foodMerge["Barcode"] = [''.join(i) for i in zip(foodMerge["Barcode"].map(str),foodMerge["Barcode_y"].map(str))]
-foodMerge.drop(foodMerge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
-######## Calories
-calMerge = calDF.merge(foodMerge, on='Name', how='right', suffixes=('', '_y'))
-calMerge.drop(calMerge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
-######## Fixing order of columns
-reorderBarcode = calMerge.pop('Barcode')
-reorderCalorie = calMerge.pop('Nutritional Data')
-calMerge.insert(0, 'Barcode', reorderBarcode)
-calMerge.insert(24, 'Nutritional Data', reorderCalorie)
-calMerge.set_index('Global ID', inplace=True)
-######## Create Completed CSV
-calMerge.to_csv('Completed.csv')
+################ Combining Data Into A Single Sheet #########################
+globalMerge = normMerge(globalDF, finalDF)          # Add Global ID data
+barcodeMerge = normMerge(barcodeDF, globalMerge)    # Add Barcode data
+lastMerge = normMerge(calDF, barcodeMerge)          # Add Calories data
+
+########################## Fixing order of columns ##########################
+reorderBarcode = lastMerge.pop('Barcode')
+reorderCalorie = lastMerge.pop('Nutritional Data')
+lastMerge.insert(0, 'Barcode', reorderBarcode)
+lastMerge.insert(24, 'Nutritional Data', reorderCalorie)
+lastMerge.set_index('Global ID', inplace=True)
+
+########################### Create Completed CSV ###########################
+lastMerge.to_csv('Completed.csv')
 print("--- Finished in %s seconds ---" % (time.time() - start_time))
