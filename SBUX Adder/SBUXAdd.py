59d99c6c86f3dccff933d21c34bfb4d47300dcea
Justin <justin.soon@verizon.net>
2021-10-27 13:44:15 -0400

    partial string match on merge

--- SBUX Adder/SBUXAdd.py
+++ SBUX Adder/SBUXAdd.py
@@ -1,16 +1,9 @@
 import pandas as pd
-import numpy as np
+from thefuzz import process
 import io
 import time
-
 start_time = time.time()
 ################ Class Objects
-class GlobalProduct:
-    itemName = ''
-    info = ''
-    def __init__(self, itemName, globalID):
-        self.itemName = itemName
-        self.info = globalID
 class BarcodeProduct:
     itemName = ''
     info = 0
@@ -23,12 +16,6 @@ class CalProduct:
     def __init__(self, itemName, size, calories):
         self.itemName = itemName
         self.info =  (size, calories)
-class PriceProduct:
-    itemName = ''
-    info = 0
-    def __init__(self, itemName, price):
-        self.itemName = itemName
-        self.info = price
         
 ################ Functions
 ######## Basic Reader
@@ -46,10 +33,19 @@ def dictStore(productArray, productDict):
         else:
             productDict[product.itemName] = [product.info]
 ######## Merge two DFs
-def normMerge(firstDF, lastDF):
-    merge = firstDF.merge(lastDF, on='Name', how='right', suffixes=('', '_y'))
-    merge.drop(merge.filter(regex='_y$').columns.tolist(),axis=1, inplace=True)
-    return merge
+def normMerge(firstDF, lastDF, data):
+    print (data + ' - Searching matches...')
+    firstDF['tempName_2'] = firstDF['Name'].apply(lambda x: process.extractOne(x, lastDF['Name'].to_list(),score_cutoff=95)) #cutoff value is percentage of match
+    tempName_list = firstDF['tempName_2'].to_list()
+    tempName_list = [_[0] if _ != None else None for _ in tempName_list]
+    firstDF['tempName_2'] = tempName_list
+    print (tempName_list)
+
+    firstDF = firstDF.merge(lastDF, left_on = 'tempName_2', right_on = 'Name', how='left', suffixes=('','_2'))
+    firstDF[data] = firstDF[data+ '_2']
+    firstDF.drop(firstDF.filter(regex='_2$').columns.tolist(),axis=1, inplace=True)
+    print (data + ' - Added!')    
+    return firstDF
 
 ################ Variables
 bevProducts = []
@@ -67,32 +63,13 @@ removeType = ['Add', 'Extra', 'Light', 'Substitute','Sub', 'No ', ' add', ' extr
     ' Modifier', ' Single', ' Double', ' Triple', 'Quad', 'Solo ', 'Doppio ', 'Triple ', ' modifier', ' modifier extra',
     ' modifier light', ' modifier no', ' modifier regular', ' modifier sub', 'Extra ', 'Light ', 'Add ']
 
-#################################### Reading CSVs ####################################
-globalContent = readCSV('CSVData/Master.csv', 1)    # Global ID - skip first row
+#################################### Reading CSVs ####################################   
 bevContent = readCSV('CSVData/BevSKU.csv', 6)       # Beverages SKU - skip first six row
 modContent = readCSV('CSVData/ModSKU.csv', 6)       # Modifier SKU - skip first six row
 foodContent = readCSV('CSVData/FoodSKU.csv', 2)     # Food SKU- skip first two row
 calContent = readCSV('CSVData/Calories.csv', 1)     # Calories Info - skip first row
-priceContent = readCSV('CSVData/Master.csv', 1)     # Prices Info - skip first row
 
 ################ Storing Data
-######## Global IDs       
-for globalLine in globalContent.split('\"\n\"'):
-    globalColumns = globalLine.split(',')
-    if len(globalColumns) > 4:
-        globalName = globalColumns[2]
-        globalID = globalColumns[0]
-        globalID = globalID.replace('\"', '')
-        globalProducts.append(GlobalProduct(globalName, globalID))
-dictStore(globalProducts, globalProdDict)
-######## Prices
-for priceLine in priceContent.split('\"\n\"'):
-    priceColumns = priceLine.split(',')
-    if len(priceColumns) > 8:
-        priceName = priceColumns[2]
-        priceItem = priceColumns[8]
-        priceProducts.append(PriceProduct(priceName, priceItem))
-dictStore(priceProducts, priceProdDict)
 ######## Beverages 
 for bevLine in bevContent.split("\n"):
     bevColumns = bevLine.split(",")
@@ -136,22 +113,6 @@ for calLine in calContent.split("\n"):
 dictStore(caloriesProducts, calProdDict)
 
 ################ Data Sort Formatting
-######## Global 
-globalOutputString = "Name,Global ID\n"
-for k,v in globalProdDict.items():
-    globalOutputString += k + ","
-    for sv in v:    
-        globalOutputString += sv
-    globalOutputString += "\n"
-globalFormatted = io.StringIO(globalOutputString)
-######## Prices
-priceOutputString = "Name,Price\n"
-for k,v in priceProdDict.items():
-    priceOutputString += k + ","
-    priceOutputString += v[0]
-    priceOutputString = priceOutputString.replace('\"', '')
-    priceOutputString += "\n"
-priceFormatted = io.StringIO(priceOutputString)
 ######## Barcodes
 barcodeOutputString = "Name,Barcode\n"
 for k,v in barcodeProdDict.items():
@@ -168,17 +129,17 @@ calOutputString = calOutputString.replace(', ',' ').replace('\') (\'',' Calories
 calFormatted = io.StringIO(calOutputString)
 
 ############################ Data into DataFrame ############################
-globalDF = pd.read_csv(globalFormatted, sep=',')        # Global
+globalDF = pd.read_csv('CSVData/master.csv', usecols = ['Name','Global ID']) #Global
+priceDF = pd.read_csv('CSVData/master.csv', usecols = ['Name','Price']) #Price
 barcodeDF = pd.read_csv(barcodeFormatted, sep=',')      # Barcodes
 calDF = pd.read_csv(calFormatted, sep=',')              # Calories
-priceDF = pd.read_csv(priceFormatted, sep=',')          # Prices
 finalDF = pd.read_csv('final.csv', encoding='UTF-8')    # Final Spreadsheet
 
 ################ Combining Data Into A Single Sheet #########################
-globalMerge = normMerge(globalDF, finalDF)          # Add Global ID data
-barcodeMerge = normMerge(barcodeDF, globalMerge)    # Add Barcode data
-calMerge = normMerge(calDF, barcodeMerge)           # Add Calories data
-lastMerge = normMerge(priceDF, calMerge)            # Add Price data    
+globalMerge = normMerge(finalDF, globalDF, 'Global ID')        # Add Global ID data
+lastMerge = normMerge(globalMerge, priceDF, 'Price')           # Add Global ID data
+barcodeMerge = normMerge(barcodeDF, globalMerge, 'Barcode')    # Add Barcode data
+calMerge = normMerge(calDF, barcodeMerge, 'Nutritional Data')  # Add Calories data
 
 ########################## Fixing order of columns ##########################
 reorderBarcode = lastMerge.pop('Barcode')
@@ -187,7 +148,7 @@ reorderPrice = lastMerge.pop('Price')
 lastMerge.insert(0, 'Barcode', reorderBarcode)
 lastMerge.insert(24, 'Nutritional Data', reorderCalorie)
 lastMerge.insert(8, 'Price', reorderPrice)
-lastMerge['Price'] = lastMerge['Price'].fillna(0)
+lastMerge['Price'] = lastMerge['Price'].fillna(0)           
 lastMerge['Price with markup'] = lastMerge['Price']
 lastMerge['Price with commission'] = lastMerge['Price']
 lastMerge['Price base'] = lastMerge['Price']
