883ed2bb0a8346268a85be304abb97821a9b659f
Justin <justin.soon@verizon.net>
2021-11-03 15:51:54 -0400

    fixing loss of data

--- Price Updater/pupdate.py
+++ Price Updater/pupdate.py
@@ -2,6 +2,25 @@ import pandas as pd, numpy as np
 from thefuzz import process
 import time
 start_time = time.time()
+
+######## getting the true price column
+def cleanVal(df):
+    df['Price'] = df.Price.mask(df.Price.str.len() == 8, df.Price.str[4:])
+    df['Price'] = df.Price.mask(df.Price.str.len() == 7, df.Price.str[3:])
+    df['Price'] = df.Price.mask(df.Price.str.len() == 6, df.Price.str[3:])
+    return df
+
+######## Fixing order of columns
+def cleanDF(df):
+    reorderPrice = df.pop('Price')
+    df.insert(8, 'Price', reorderPrice)
+    df['Price'] = df['Price'].fillna(0)           
+    df['Price with markup'] = df['Price']
+    df['Price with commission'] = df['Price']
+    df['Price base'] = df['Price']
+    df.set_index('Global ID', inplace=True)
+    return df
+
 ######## Merge two DFs
 def normMerge(firstDF, lastDF, data):
     # Searching for string match
@@ -14,7 +33,7 @@ def normMerge(firstDF, lastDF, data):
     firstDF = firstDF.merge(lastDF, left_on = 'tempName_2', right_on = 'Name', how='left', suffixes=('','_2'))
     firstDF = firstDF.replace(np.nan, '')
     firstDF["Price"] = [''.join(i) for i in zip(firstDF["Price"].map(str),firstDF["Price_2"].map(str))]
-    firstDF['Price'] = firstDF.Price.mask(firstDF.Price.str.len() > 4, firstDF.Price.str[4:])
+    cleanVal(firstDF)
     firstDF.drop(firstDF.filter(regex='_2$').columns.tolist(),axis=1, inplace=True)
     print (data + ' - Added!')    
     return firstDF
@@ -24,16 +43,7 @@ currentDF = pd.read_csv('current.csv', encoding='UTF-8')
 priceDF = pd.read_csv('final.csv', usecols = ['Name','Price']) #Price
 ################ Combining Data Into A Single Sheet #########################
 merge = normMerge(currentDF, priceDF, 'Price')        # Add Global ID data
-
-########################## Fixing order of columns ##########################
-reorderPrice = merge.pop('Price')
-merge.insert(8, 'Price', reorderPrice)
-merge['Price'] = merge['Price'].fillna(0)           
-merge['Price with markup'] = merge['Price']
-merge['Price with commission'] = merge['Price']
-merge['Price base'] = merge['Price']
-merge.set_index('Global ID', inplace=True)
-
+cleanDF(merge)
 ########################### Create Completed CSV ###########################
 merge.to_csv('finalAdded.csv')
 print("--- Finished in %s seconds ---" % (time.time() - start_time))
