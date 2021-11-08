38f3edd93f23b90bf06ab9d4278afde3b74e89fe
Justin <justin.soon@verizon.net>
2021-11-07 23:51:09 -0500

    Update and rename savemod.js to savebutton.js

--- Menu Tools/savebutton.js
+++ Menu Tools/savebutton.js
@@ -2,7 +2,7 @@
 // @name         Starship Save Button
 // @namespace    Save Shortcut
 // @version      0.1
-// @description  Save button shortcut for the modifier page press F2
+// @description  Save button shortcut for the modifier page press F2 and F4 for items page
 // @author       Justin Soon
 // @match        https://panel.starship.xyz/marketplace/serviceassignments/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -12,6 +12,13 @@
 window.addEventListener('keyup', function (e) {
   if (e.keyCode === 113) {
      var button = document.evaluate("//*[@id='modifiers']/div/div[2]/div/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+     button.click();
+  }
+}, false);
+
+window.addEventListener('keyup', function (e) {
+  if (e.keyCode === 115) {
+     var button = document.evaluate("/html/body/div[1]/main/div/form/div/div[1]/div/div/div[19]/div/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
      button.click();
   }
 }, false);
