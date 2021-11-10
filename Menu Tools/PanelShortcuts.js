d50f7a208b9d4ec4d05f95b021f088453cefb7b7
Justin <justin.soon@verizon.net>
2021-11-10 14:40:57 -0500

    bug fix v3

--- Menu Tools/PanelShortcuts.js
+++ Menu Tools/PanelShortcuts.js
@@ -2,7 +2,7 @@
 // @name         Starship Panel Shortcuts
 // @namespace    Panel Shortcuts
 // @version      0.5
-// @description  Shortcut for the modifier or item page press F2, Tab to access item modifiers from an item's page, Alt + Q = Items, Alt + W = Item Categories, Alt + 1 = Action Button
+// @description  Shortcut for the modifier or item page press F2, Tab to access item modifiers from an item's page, Alt + Q = Items, Alt + W = Item Categories, Alt + 1 = Action Button, Alt + 2 = Download, Alt + 3 = Upload
 // @author       Justin Soon
 // @match        https://panel.starship.xyz/marketplace/serviceassignments/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -27,38 +27,38 @@ window.addEventListener('keyup', function (e) {
   }
 }, false)
 
-window.addEventListener('keyup', function (e) {
-    if (e.keyCode == altKey || e.keyCode == qKey) {
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode.altKey || e.keyCode == qKey) {
         var button = document.evaluate("/html/body/div[1]/main/div/ul/li[2]/a", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         button.click();
     }
 }, false)
-window.addEventListener('keyup', function (e) {
-    if (e.keyCode == altKey || e.keyCode == wKey) {
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode.altKey || e.keyCode == wKey) {
         var button = document.evaluate("/html/body/div[1]/main/div/ul/li[3]/a", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         button.click();
     }
 }, false)
 
 if (/\items\b/.test (location.pathname) ) {
-    window.addEventListener('keyup', function (e) {
-        if (e.keyCode == altKey || e.keyCode == oneKey) {
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode.altKey || e.keyCode == oneKey) {
             var button = document.evaluate("/html/body/div[1]/nav/div/div[2]/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
             button.click();
         }
     }, false)
 }
 if (/\items\b/.test (location.pathname) ) {
-    window.addEventListener('keyup', function (e) {
-        if (e.keyCode == altKey || e.keyCode == twoKey) {
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode.altKey || e.keyCode == twoKey) {
             var button = document.evaluate("//*[@id='download-items-csv']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
             button.click();
         }
     }, false)
 }
 if (/\items\b/.test (location.pathname) ) {
-    window.addEventListener('keyup', function (e) {
-        if (e.keyCode == altKey || e.keyCode == threeKey) {
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode.altKey || e.keyCode == threeKey) {
             var button = document.evaluate("/html/body/div[1]/main/div/div[1]/div/div[1]/div/label[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
             button.click();
         }
