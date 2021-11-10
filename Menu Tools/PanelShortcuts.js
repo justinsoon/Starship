403eb3532afb71258e0e686d4c61ba5b9f1f1e31
Justin <justin.soon@verizon.net>
2021-11-10 11:15:46 -0500

    Added action button shortcut

--- Menu Tools/PanelShortcuts.js
+++ Menu Tools/PanelShortcuts.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Starship Panel Shortcuts
 // @namespace    Panel Shortcuts
-// @version      0.3
-// @description  Shortcut for the modifier or item page press F2, Tab to access item modifiers from an item's page, Alt + Q = Items, Alt + W = Item Categories
+// @version      0.5
+// @description  Shortcut for the modifier or item page press F2, Tab to access item modifiers from an item's page, Alt + Q = Items, Alt + W = Item Categories, Alt + 1 = Action Button
 // @author       Justin Soon
 // @match        https://panel.starship.xyz/marketplace/serviceassignments/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -14,6 +14,7 @@ var ctrlDown = false,
     cmdKey = 91,
     qKey = 81,
     wKey = 87,
+    oneKey = 49,
     tabKey = 9,
     f2Key = 113;
 
@@ -37,6 +38,14 @@ window.addEventListener('keyup', function (e) {
     }
 }, false)
 
+if (/\items\b/.test (location.pathname) ) {
+    window.addEventListener('keyup', function (e) {
+        if (e.keyCode == altKey || e.keyCode == oneKey) {
+            var button = document.evaluate("/html/body/div[1]/nav/div/div[2]/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            button.click();
+        }
+    }, false)
+}
 if (/\bmodifiers\b/.test (location.pathname) ) {
     window.addEventListener('keyup', function (e) {
         if (e.keyCode == f2Key) {
